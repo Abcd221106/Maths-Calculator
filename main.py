312b511546b05279aeffeafffd8ed2c54afc45ef
Abcd221106 <shravan@gmail.com>
2022-02-04 17:56:41 +0530

    added add method

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 ##githb token: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
-def main():{
-    print("Hello World")
-}
+def add(num1, num2):
+    result = num1+num2
+    print(result)
+print("Input first number:")
+num1 =input()
+print("Input Second number:")
+num2 =input()
+add(num1, num2)
 
-main()
+
+
+  
